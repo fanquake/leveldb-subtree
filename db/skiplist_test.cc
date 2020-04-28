f1f0d18c9edcb384ab63bc33ea5029bd0c44d7d9
Victor Costan <costan@google.com>
2020-04-28 16:41:33 +0000

    Fix C++11 build.
    PiperOrigin-RevId: 308839805

--- db/skiplist_test.cc
+++ db/skiplist_test.cc
@@ -282,6 +282,9 @@ class ConcurrentTest {
   }
 };
 
+// Needed when building in C++11 mode.
+constexpr uint32_t ConcurrentTest::K;
+
 // Simple test that does single-threaded testing of the ConcurrentTest
 // scaffolding.
 TEST(SkipTest, ConcurrentWithoutThreads) {

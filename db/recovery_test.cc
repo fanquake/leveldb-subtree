google/leveldb

Remove the `/` prefix from the recovery_test test file to prevent a double `/`.
PiperOrigin-RevId: 388341429

db/recovery_test.cc
@@ -18,7 +18,7 @@ namespace leveldb {
 class RecoveryTest : public testing::Test {
  public:
   RecoveryTest() : env_(Env::Default()), db_(nullptr) {
-    dbname_ = testing::TempDir() + "/recovery_test";
+    dbname_ = testing::TempDir() + "recovery_test";
     DestroyDB(dbname_, Options());
     Open();
   }

fanquake/leveldb-subtree

db/recovery_test.cc
@@ -18,7 +18,7 @@ namespace leveldb {
 class RecoveryTest {
  public:
   RecoveryTest() : env_(Env::Default()), db_(nullptr) {
-    dbname_ = test::TmpDir() + "/recovery_test";
+    dbname_ = test::TmpDir() + "recovery_test";
     DestroyDB(dbname_, Options());
     Open();
   }